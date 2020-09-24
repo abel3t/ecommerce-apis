d20c6d4b440bfc59d706b0ad3f0ddb8ebccdefb9
Abel Tran <abeltran.develop@gmail.com>
2020-09-24 22:22:34 +0700

    Edit model

--- src/Database/Models/User.ts
+++ src/Database/Models/User.ts
@@ -1,58 +1,7 @@
 import { model, Schema } from 'mongoose';
 
-//region Enums
-export enum AddressTypes {
-  HomeApartment,
-  OrganCompany
-}
-
-//endregion
-
-//region Interfaces
-export interface IUser {
-  name: string,
-  userName: string,
-  email: string,
-  hash: string,
-  salt: string,
-  profilePictureUrl?: string,
-  deleted?: boolean,
-  active?: boolean,
-  lastLogin?: Date,
-  lastActivity?: Date,
-  createdAt?: Date,
-  UpdatedAt?: Date,
-  roles?: Array<IRole>,
-  shoppingCartItems?: Array<IShoppingCartItem>,
-  shippingAddresses?: Array<IShippingAddress>
-}
-
-interface IRole {
-  name: string,
-  freeShipping?: boolean,
-  taxExempt?: boolean,
-  active?: boolean,
-  minOrderAmount?: number,
-  maxOrderAmount?: number
-}
-
-interface IShoppingCartItem {
-  productId: string,
-  quantity: number,
-  createdAt?: Date
-}
-
-export interface IShippingAddress {
-  address: string;
-  districtTown: string;
-  name: string;
-  phone: string;
-  provinceCity: string;
-  type: AddressTypes;
-  wardCommune: string;
-}
-
-//endregion
+import { IUser } from 'Database/Interfaces';
+import logger from 'Core/Logger';
 
 const schema = new Schema<IUser>({
   name: { type: Schema.Types.String, trim: true, required: true },
@@ -107,7 +56,7 @@ export default class User {
     await this.save(user)
       .then(data => result = data as IUser)
       .catch(error => {
-        console.log(error);
+        logger.info(error);
         throw new Error('Save user error!');
       });
 
