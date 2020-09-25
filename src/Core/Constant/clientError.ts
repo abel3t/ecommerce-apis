c461963bf635266282cbc940ae614a30393c54ac
Abel Tran <abeltran.develop@gmail.com>
2020-09-26 01:18:20 +0700

    Add constants

--- src/Core/Constant/clientError.ts
+++ src/Core/Constant/clientError.ts
@@ -1,3 +1,4 @@
 export const USER_EXISTED = 'User is existed!';
 export const WRONG_EMAIL_PASSWORD = 'Wrong email or password!';
 export const SAVE_USER_ERROR = 'Save user error!';
+export const INVALID_TOKEN = 'Invalid token!';
