f6b064fef1aaefa4d27bd37cc5b0b9f4d30f8f32
Abel Tran <abeltran.develop@gmail.com>
2020-09-27 00:02:33 +0700

    Update Auth

--- src/Controllers/Auth/register.ts
+++ src/Controllers/Auth/register.ts
@@ -1,23 +1,26 @@
 import crypto from 'crypto';
 
-import { IUser } from 'Database/Interfaces';
+import { IUser, IUserSignUp } from 'Database/Interfaces';
 import { User } from 'Database/Models';
+import { ROLES } from 'Core/Constants/common';
 
 interface IRegisterInput {
   name: string,
   userName: string
   email: string
   password: string
+  role: string
 }
 
-export async function register(_: any, { name, userName, email, password }: IRegisterInput): Promise<IUser | null> {
+export async function register(_: any, { name, userName, email, password, role = ROLES.GUESTS }: IRegisterInput): Promise<IUser | null> {
 
   const salt: string = crypto.randomBytes(16).toString('hex');
   const hash: string = crypto.pbkdf2Sync(password, salt, 10000, 512, 'sha512').toString('hex');
-  const user: IUser = {
+  const user: IUserSignUp = {
     name,
     userName,
     email: email.toLowerCase(),
+    role,
     hash,
     salt
   };
--- src/Controllers/Auth/signIn.ts
+++ src/Controllers/Auth/signIn.ts
@@ -1,8 +1,10 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 import crypto from 'crypto';
 
+import logger from 'Core/Logger';
 import { User } from 'Database/Models';
-import { WRONG_EMAIL_PASSWORD } from 'Core/Constants';
 import { generateJwt } from 'Core/Jwt';
+import { WRONG_EMAIL_PASSWORD } from 'Core/Constants';
 
 interface ISignInInput {
   email: string,
@@ -16,20 +18,48 @@ export async function signIn(_: any, { email, password }: ISignInInput) {
     throw new Error(WRONG_EMAIL_PASSWORD);
   }
 
-  const token = generateJwt({
-    userId: existedUser._id,
+  const customToken = await generateJwt({
+    userId: `${existedUser._id}`,
     role: existedUser.role
   });
+
+  const { idToken: token, refreshToken } = await exchangeToken(customToken);
 
   return {
     name: existedUser.name,
     email: existedUser.email,
     userName: existedUser.userName,
     role: existedUser.role,
-    token
+    token,
+    refreshToken: refreshToken
   };
 }
 
 function isEqualPassword(password: string, hash: string, salt: string) {
   return hash === crypto.pbkdf2Sync(password, salt, 10000, 512, 'sha512').toString('hex');
+}
+
+/**
+ *
+ * @param {string} token
+ * @returns {Promise<{}>}
+ */
+async function exchangeToken(token: string) {
+  const config: AxiosRequestConfig = {
+    method: 'POST',
+    url: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${process.env.FIREBASE_WEB_KEY}`,
+    data: {
+      returnSecureToken: true,
+      token
+    }
+  };
+
+  return await axios(config)
+    .then((response: AxiosResponse) => {
+      return response?.data;
+    })
+    .catch((error: AxiosError) => {
+      logger.error(new Error(JSON.stringify(error.response?.data)));
+      throw error;
+    });
 }
